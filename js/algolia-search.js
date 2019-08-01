f73509ee0922a68fcc2c92c2afe4a35fa9034cd2
黄佩雁 <huangpeiyan@zuzuche.com>
2019-08-01 11:32:38 +0800

    Site updated: 2019-08-01 11:32:38

--- js/algolia-search.js
+++ js/algolia-search.js
@@ -37,12 +37,18 @@ $(document).ready(function() {
       templates  : {
         item: function(data) {
           var link = data.permalink ? data.permalink : CONFIG.root + data.path;
-          return `<a href="${link}" class="algolia-hit-item-link">${data._highlightResult.title.value}</a>`;
+          return (
+            '<a href="' + link + '" class="algolia-hit-item-link">'
+          + data._highlightResult.title.value
+          + '</a>'
+          );
         },
         empty: function(data) {
-          return `<div id="algolia-hits-empty">
-              ${algoliaSettings.labels.hits_empty.replace(/\$\{query}/, data.query)}
-            </div>`;
+          return (
+            '<div id="algolia-hits-empty">'
+          + algoliaSettings.labels.hits_empty.replace(/\$\{query}/, data.query)
+          + '</div>'
+          );
         }
       },
       cssClasses: {
@@ -57,11 +63,13 @@ $(document).ready(function() {
           var stats = algoliaSettings.labels.hits_stats
             .replace(/\$\{hits}/, data.nbHits)
             .replace(/\$\{time}/, data.processingTimeMS);
-          return `${stats}
-            <span class="algolia-powered">
-              <img src="${CONFIG.root}images/algolia_logo.svg" alt="Algolia"/>
-            </span>
-            <hr/>`;
+          return (
+            stats
+            + '<span class="algolia-powered">'
+            + '  <img src="' + CONFIG.root + 'images/algolia_logo.svg" alt="Algolia" />'
+            + '</span>'
+            + '<hr />'
+          );
         }
       }
     }),
@@ -97,17 +105,10 @@ $(document).ready(function() {
     $('#algolia-search-input').find('input').focus();
   });
 
-  function onPopupClose() {
+  $('.popup-btn-close').click(function() {
     $('.popup').hide();
     $('.algolia-pop-overlay').remove();
     $('body').css('overflow', '');
-  }
-  $('.popup-btn-close').click(onPopupClose);
-
-  $(document).on('keyup', function(event) {
-    var shouldDismissSearchPopup = event.which === 27 && $('.search-popup').is(':visible');
-    if (shouldDismissSearchPopup) {
-      onPopupClose();
-    }
   });
+
 });
